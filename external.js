6ff9f74ff43344cda75d8f0c69fdb05d942124e0
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-07-12 05:58:16 -0500

    created the base of main branch

---
+++ external.js
@@ -0,0 +1,7 @@
+for (i=0; i<256; i++){
+    const divs = document.createElement("div");
+    divs.className ="square";
+    const container = document.querySelector("#container")
+    container.append(divs);
+}
+
