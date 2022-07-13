98bd262301f71d5b3c57dd99152bd674f68793cf
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-07-13 06:53:46 -0500

    enabled mouseover/mouseout event in JS

--- external.js
+++ external.js
@@ -1,7 +1,16 @@
+// created 256 divs with class name "square" under the div with ID = container by using a for loop. 
 for (i=0; i<256; i++){
     const divs = document.createElement("div");
     divs.className ="square";
     const container = document.querySelector("#container")
     container.append(divs);
+    container.onmouseover = function(event) {
+        let target = event.target;
+        target.style.background = 'pink';
+      };
+      
+    container.onmouseout = function(event) {
+        let target = event.target;
+        target.style.background = 'blue';
+      };
 }
-
