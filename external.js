2446cdd61a27511f12b6e3a672623bdc3db955ac
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-07-25 20:27:33 -0500

    implemented a function to make changing grids more efficient

--- external.js
+++ external.js
@@ -1,21 +1,68 @@
 // created 256 divs with class name "square" under the div with ID = container by using a for loop. 
-for (i=0; i<256; i++){
-    const divs = document.createElement("div");
-    divs.className ="square";
-    const container = document.querySelector("#container")
-    container.append(divs);
-    container.onmouseover = function(event) {
-        let target = event.target;
-        target.style.background = 'pink';
+function makeGrid(row,column){
+    for (i=0; i<row*column; i++){
+        const divs = document.createElement("div");
+        divs.className ="square";
+        const container = document.querySelector("#container")
+        container.append(divs);
+        container.onmouseover = function(event) {
+            let target = event.target;
+            target.style.background = 'pink';
       };
       
-    container.onmouseout = function(event) {
-        let target = event.target;
-        target.style.background = 'blue';
+        container.onmouseout = function(event) {
+            let target = event.target;
+            target.style.background = 'blue';
       };
 }
-
+}
+makeGrid(16,16);
+/*
 const button = document.querySelector("#size");
 button.addEventListener("click", function () {
     let playerOne = window.prompt ("What size grid would you like?");
+    return playerOne;
+    console.log(playerOne);
 });
+
+/*if(button == 100){
+    for (i=0; i<100; i++){
+        const divs = document.createElement("div");
+        divs.className ="square";
+        const container = document.querySelector("#container")
+        container.append(divs);
+        container.onmouseover = function(event) {
+            let target = event.target;
+            target.style.background = 'pink';
+          };
+          
+        container.onmouseout = function(event) {
+            let target = event.target;
+            target.style.background = 'blue';
+          };
+    }
+}
+else{
+    for (i=0; i<256; i++){
+        const divs = document.createElement("div");
+        divs.className ="square";
+        const container = document.querySelector("#container")
+        container.append(divs);
+        container.onmouseover = function(event) {
+            let target = event.target;
+            target.style.background = 'pink';
+          };
+          
+        container.onmouseout = function(event) {
+            let target = event.target;
+            target.style.background = 'blue';
+          };
+    }
+}*/
+
+/*function changeGrid (){
+    let userInput = Number(window.prompt ("What grid size would you like?", 16));
+    return userInput;  
+    console.log(userInput);
+}
+ changeGrid();*/
