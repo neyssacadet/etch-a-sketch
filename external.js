d8f36bba710c0815b416175ec303a62b9a7b2a55
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-07-30 09:57:02 -0500

    inserted CSS number of rows and column into JS

--- external.js
+++ external.js
@@ -1,5 +1,8 @@
 function makeGrid(row,column){
+    container.style.gridTemplateColumns = (`repeat(${column}, 50px)`);
+    container.style.gridTemplateRows = (`repeat(${row}, 50px)`);
     for (i=0; i<row*column; i++){
+        row = column;
         const divs = document.createElement("div");
         divs.className ="square";
         const container = document.querySelector("#container")
@@ -20,48 +23,6 @@ function sizeit () {
     let userInput = Number(window.prompt ("What grid size would you like?"));
     makeGrid(userInput,userInput);
 }
- 
-/*
-const button = document.querySelector("#size");
-button.addEventListener("click", function () {
-    let playerOne = window.prompt ("What size grid would you like?");
-    return playerOne;
-    console.log(playerOne);
-});
 
-/*if(button == 100){
-    for (i=0; i<100; i++){
-        const divs = document.createElement("div");
-        divs.className ="square";
-        const container = document.querySelector("#container")
-        container.append(divs);
-        container.onmouseover = function(event) {
-            let target = event.target;
-            target.style.background = 'pink';
-          };
-          
-        container.onmouseout = function(event) {
-            let target = event.target;
-            target.style.background = 'blue';
-          };
-    }
-}
-else{
-    for (i=0; i<256; i++){
-        const divs = document.createElement("div");
-        divs.className ="square";
-        const container = document.querySelector("#container")
-        container.append(divs);
-        container.onmouseover = function(event) {
-            let target = event.target;
-            target.style.background = 'pink';
-          };
-          
-        container.onmouseout = function(event) {
-            let target = event.target;
-            target.style.background = 'blue';
-          };
-    }
-}*/
 
 
