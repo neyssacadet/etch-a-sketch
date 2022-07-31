dc87779e7f66fad6e80bbb76eb2365f9df3684d7
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-07-31 08:06:35 -0500

    set CSS grid layout to specific size where size of cells expand or shrink to fit

--- external.js
+++ external.js
@@ -1,6 +1,6 @@
 function makeGrid(row,column){
-    container.style.gridTemplateColumns = (`repeat(${column}, 50px)`);
-    container.style.gridTemplateRows = (`repeat(${row}, 50px)`);
+    container.style.gridTemplateColumns = (`repeat(${column}, 2fr`);
+    container.style.gridTemplateRows = (`repeat(${row}, 2fr`);
     for (i=0; i<row*column; i++){
         row = column;
         const divs = document.createElement("div");
