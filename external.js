70a21285e068565f6f182ebad370159f6ee10525
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-08-02 07:12:58 -0500

    create a clear function that will clear up grid once game is loaded

--- external.js
+++ external.js
@@ -1,28 +1,34 @@
-function makeGrid(row,column){
+//Initialization 
+let defaultSize = 16;
+
+//function to create a grid 
+function createCustomGrid(row,column){
     container.style.gridTemplateColumns = (`repeat(${column}, minmax(0.5px, 1fr)`);
     container.style.gridTemplateRows = (`repeat(${row}, minmax(0.5px, 1fr)`);
     for (i=0; i<row*column; i++){
-        row = column;
         const divs = document.createElement("div");
         divs.className ="square";
         const container = document.querySelector("#container")
-        container.append(divs);
-        container.onmouseover = function(event) {
-            let target = event.target;
-            target.style.background = 'pink';
-      };
-      
+        container.append(divs);     
         container.onmouseout = function(event) {
             let target = event.target;
             target.style.background = 'blue';
       };
-}
+    }
 }
 
+//function that makes button clickable 
 function sizeit () {
     let userInput = Number(window.prompt ("What grid size would you like?"));
-    makeGrid(userInput,userInput);
+    clearGrid();
+    createCustomGrid(userInput,userInput);
 }
 
+function clearGrid(){
+    container.innerHTML = "";
+}
 
-
+//this is the default 16x16 once game is loaded. 
+window.onload = () => {
+    createCustomGrid(defaultSize,defaultSize);
+}
